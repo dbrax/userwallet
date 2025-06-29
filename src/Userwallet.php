24a29f05893514fcdd930f9087a2ec58230e44b3
Emmanuel Mnzava <epmnzava@gmail.com>
2025-06-29 22:10:28 +0300

    updated code

--- src/Userwallet.php
+++ src/Userwallet.php
@@ -76,7 +76,7 @@ class Userwallet
         if ($this->userHasWallet($userid)) {
             $wallet = Wallet::find(Wallet::where('userid', $userid)->first()->id);
 
-            if (number_format($wallet->balance) < number_format($amount))
+            if (number_format($wallet->balance) < number_format($amount,2,'.',""))
                 return ["status" => 0, "ledger" => [], "wallet" => [], "message" => "Amount to be withdrawned is greater than balance"];
 
             $balance = $wallet->balance - $amount;
